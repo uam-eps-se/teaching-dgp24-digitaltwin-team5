c1bee9231ffd1f9479565e2357d85c84bfe68e8b
Javier Wang Zhou <60742676+Acervans@users.noreply.github.com>
2024-10-24 23:12:42 +0200

    Revert "feature: visualize available rooms (frontend) #75"

--- storm-ui/src/app/(dashboard)/rooms/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-export async function generateMetadata({ params }: { params: { id: string } }) {
-  return {
-    title: params.id,
-  }
-}
-
-export default function Page({ params }: { params: { id: string } }) {
-  return <div>Room ID: {params.id}</div>
-}
