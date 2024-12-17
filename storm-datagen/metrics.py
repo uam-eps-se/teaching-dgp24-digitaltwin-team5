6176767cd16b89c9e9391a115f0f69fa7958e568
Javier Wang Zhou <60742676+Acervans@users.noreply.github.com>
2024-12-17 19:59:37 +0100

    feat: datagen adapted for production

--- storm-datagen/metrics.py
+++ storm-datagen/metrics.py
@@ -90,7 +90,16 @@ def update_temperature(tt, time) -> dict:
     return temp
 
 
-load_dotenv(".env")
+if len(sys.argv) == 1:
+    load_dotenv(".env")
+elif sys.argv[1] == "--prod":
+    load_dotenv(".env.production")
+else:
+    print(
+        f"Usage: python3 {sys.argv[0]}\n",
+        "\t--prod (optional): Generate data for deployed application",
+    )
+    sys.exit()
 
 METRICS_URL = os.getenv("METRICS_ENDPOINT")
 ROOMS_URL = os.getenv("ROOMS_ENDPOINT")
